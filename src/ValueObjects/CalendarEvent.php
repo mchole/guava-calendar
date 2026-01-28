b1edc9f97daa19184b96d5b38f9cbc669abeedca
mchole <mchole@users.noreply.github.com>
2026-01-28 12:23:10 +0000

    Improve title handling for calendar events
    Update title handling in fromCalendarObject method to support HtmlString.

--- src/ValueObjects/CalendarEvent.php
+++ src/ValueObjects/CalendarEvent.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use Illuminate\Support\HtmlString;
 
 use function Guava\Calendar\utc_to_user_local_time;
 
@@ -359,8 +360,14 @@ class CalendarEvent
 
     public function fromCalendarObject(array $data, int $timezoneOffset, bool $useFilamentTimezone): static
     {
+        $title = $data['title'];
+        if(is_array($data['title']) && $data['title']['html']) {
+            $title = new HtmlString($data['title']['html']);
+        }
+        
+        
         $this
-            ->title($data['title'])
+            ->title($title)
             ->start(utc_to_user_local_time($data['start'], $timezoneOffset, $useFilamentTimezone))
             ->end(utc_to_user_local_time($data['end'], $timezoneOffset, $useFilamentTimezone))
             ->allDay($data['allDay'])
